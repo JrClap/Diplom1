c2d873260cd4f049a0882b88c2a77d849b8e7648
Uladzimir <blind72@mail.ru>
2023-03-06 20:07:05 +0100

    Added improved functions CheckArray

--- task2/Program.cs
+++ task2/Program.cs
@@ -33,7 +33,7 @@ void CheckArray(string[] array)
     Console.Write("An array of values with more than 3 elements: ");
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length > 3)
+        if (array[i].Length <= 3)
         {
             Console.Write(array[i] + " ");
         }
