1a9f03af6088f52408ab68c52084728df812c597
Uladzimir <blind72@mail.ru>
2023-03-06 17:53:57 +0100

    Added array input code from user via terminal

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,3 +2,44 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 //При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
 
+string[] CreateArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Enter element {i + 1}: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+// void CheckArray(string[] array)
+// {
+//     Console.Write("An array of values with more than 3 elements: ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i].Length > 3)
+//         {
+//             Console.Write(array[i] + " ");
+//         }
+//     }
+//     Console.WriteLine();
+// }
+
+Console.Write("Enter the size of the array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] myArray = CreateArray(size);
+Console.Write("The introduced array looks like: ");
+ShowArray(myArray);
+//CheckArray(myArray);
